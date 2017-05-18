c87e4079ee1d008f537dca7ef808cbb56ef618d3
Inyoung <wddjiy777@gmail.com>
2017-05-18 12:52:29 +0900

    update(js)

---
+++ JavaScript/testSubstring.js
@@ -0,0 +1,11 @@
+/**
+ * Created by alleysintern on 2017. 5. 16..
+ */
+var name = '53Y8wHEYyIm4ecrnsA202g'
+
+console.log(name.substring(0, 2))
+console.log(name.substring(2))
+
+console.log(name.length)
+
+console.log(name.substring(0, 2))
